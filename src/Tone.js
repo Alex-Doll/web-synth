2dd377a1b6fd57a40cbc6ee94f82d4bdc20f6cb3
rosco1010 <doll3098@ou.edu>
2018-12-30 21:55:13 -0600

    Store the osc in the Tone's local state and create/destroy the osc when the key is pressed

--- src/Tone.js
+++ src/Tone.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 
 class Tone extends Component {
-  componentDidMount() {
-    const osc = new OscillatorNode(this.props.ctx, {
-      frequency: this.props.freq,
-      type: this.props.waveType,
-    });
-    osc.start();
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      osc: null,
+    };
+  }
+
+  componentDidMount() {
     document.addEventListener('keydown', (event) => {
-      if (event.key === this.props.triggerKey) {
+      if (event.key === this.props.triggerKey && !this.state.osc) {
+        const osc = new OscillatorNode(this.props.ctx, {
+          frequency: this.props.freq,
+          type: this.props.waveType,
+        });
+        osc.start();
         osc.connect(this.props.ctx.destination);
+        this.setState({ osc });
       }
     });
 
     document.addEventListener('keyup', (event) => {
-      if (event.key === this.props.triggerKey) {
-        osc.disconnect();
+      if (event.key === this.props.triggerKey && this.state.osc) {
+        this.state.osc.disconnect();
+        this.setState({ osc: null });
       }
     });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      console.log(this.props);
+    }
   }
 
   render() {
