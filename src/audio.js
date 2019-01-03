07ae769d5e60779017da9bdf6594784fcb9dbd08
rosco1010 <doll3098@ou.edu>
2019-01-03 14:27:24 -0600

    Create a Tone class for oscillator sounds

--- src/audio.js
+++ src/audio.js
@@ -4,3 +4,18 @@ export const audioContext = window.AudioContext ? new window.AudioContext() : ne
 
 export const masterGainNode = audioContext.createGain();
 masterGainNode.gain.value = INITIAL_MASTER_GAIN;
+
+
+export class Tone {
+  constructor(frequency = 440, detune = 0, type = 'sine') {
+    this.frequency = frequency;
+    this.detune = detune;
+    this.type = type;
+
+    this.osc = new OscillatorNode(audioContext, {
+      frequency,
+      detune,
+      type,
+    });
+  }
+}
